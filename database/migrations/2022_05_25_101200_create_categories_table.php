d060b96f477765f6b208eb7469a3f1b542deb8ad
codeaxion <codeaxion77@gmail.com>
2022-05-25 19:20:30 +0530

    inserted data into migrations in categories and product table also added relationship with categories

--- database/migrations/2022_05_25_101200_create_categories_table.php
+++ database/migrations/2022_05_25_101200_create_categories_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->string('short_description');
             $table->timestamps();
         });
     }
--- database/migrations/2022_05_25_101301_create_products_table.php
+++ database/migrations/2022_05_25_101301_create_products_table.php
@@ -15,7 +15,26 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('brand_name');
+            $table->string('slug')->unique();
+            $table->string('short_description')->nullable();
+            $table->text('description');
+            $table->text('product_details');
+            $table->decimal('regular_price');
+            $table->decimal('sale_price')->nullable();
+            $table->string('SKU');
+            $table->integer('rec_active')->default(-1);
+            $table->integer('stock_status')->default(-1);
+            $table->string('barcode')->nullable();
+            $table->boolean('featured')->default(false);
+            $table->string('image')->nullable();
+            $table->text('images')->nullable();
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedInteger('quantity')->default(10);
             $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            
         });
     }
 
